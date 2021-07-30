2c8612260bcd7300c721d60fcf65d42e1d17af65
BiggestBluebird <bluebirdtrucks@gmail.com>
2021-07-30 09:03:23 -0500

    Add files via upload

---
+++ src/main/java/com/attendtrack/AttendanceTrackerBE/repository/EventRepository.java
@@ -0,0 +1,14 @@
+package com.attendtrack.AttendanceTrackerBE.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.attendtrack.AttendanceTrackerBE.model.Event;
+
+public interface EventRepository extends JpaRepository<Event, Long> {
+
+	List<Event> findByPublished(boolean published);
+
+	List<Event> findByTitleContaining(String title);
+}
---
+++ src/test/java/com/attendtrack/AttendanceTrackerBE/AttendanceTrackerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attendtrack.AttendanceTrackerBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceTrackerBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
